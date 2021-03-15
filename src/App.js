63c72c988b7e36abe038e6a561942f43bafe0f66
MilosDraskovic <milosffh@yahoo.com>
2021-03-15 21:20:56 +0100

    Finished static coding for the accordion component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+import Accordion from "./components/accordion/Accordion";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>React UI Components</h1>
+      <Accordion />
     </div>
   );
 }
