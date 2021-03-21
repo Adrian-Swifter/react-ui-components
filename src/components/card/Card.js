14f2313750e5c82cc6286f7f1dee6df12e86c0cc
MilosDraskovic <milosffh@yahoo.com>
2021-03-21 21:22:48 +0100

    Added style to card component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,21 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faIdCard } from "@fortawesome/free-solid-svg-icons";
+
 function Card() {
   return (
     <div className="card_wrapper">
-      <header>
-        <div className="card_image_wrapper">icon</div>
+      <header className="card_header">
+        <div className="card_image_wrapper">
+          <FontAwesomeIcon icon={faIdCard} color="#ce82ff" size="2x" />
+        </div>
         <div className="card_header_text_wrapper">
-          <h2>Card text header title prmary text</h2>
-          <h3>Secondary text</h3>
+          <h3>Title primary text</h3>
+          <p>Secondary text</p>
         </div>
       </header>
       <main>
         <img
-          style={{ width: "300px" }}
+          style={{ maxWidth: "100%" }}
           src="https://images.unsplash.com/photo-1526660690293-bcd32dc3b123?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw"
           alt="puppy"
         />
       </main>
-      <footer>
+      <footer className="card_footer">
         <p className="card_footer_text">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry.
