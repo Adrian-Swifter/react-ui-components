2500b24df8da73107007940cd718b10801fd8b66
MilosDraskovic <milosffh@yahoo.com>
2021-03-23 20:58:45 +0100

    Addedd useState to fetch data from reqres api Deleted fetch components and state put into App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./styles.css";
 import Accordion from "./components/accordion/Accordion";
 import Card from "./components/card/Card";
-import Fetch from "./components/Fetch"
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [users, usersSet] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const fullResponse = await fetch("https://reqres.in/api/users");
+      const responseJson = await fullResponse.json();
+      usersSet(responseJson.data);
+    }
+
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
       <h1>React UI Components</h1>
       <Accordion />
-      <Card />
-      <Fetch />
+      {users.map((user) => (
+        <Card
+          firstName={user.first_name}
+          lastName={user.last_name}
+          email={user.email}
+          avatar={user.avatar}
+        />
+      ))}
     </div>
   );
 }
